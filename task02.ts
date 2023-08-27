7367d8a011c11193f3ebcdb9b15bc0bc812c4302
daudiqbal951 <daudiqbal951@yahoo.com>
2023-08-27 10:03:38 +0500

    Updated

--- task02.ts
+++ task02.ts
@@ -1,10 +1,11 @@
 //Implement a program that uses a for loop to iterate through an array of
 //numbers and print only the even numbers.
 
-var array:number[]=[1,2,3,4,5,6,7,8,9,10]
+var array:number[]=[1,1,2,4,4,5,6,7,8,9,10]
 for(var i=1; i<=array.length;i++){
     if(array[i-1]%2!=0){
     array.splice(i-1,1)
+   i--
     }
 }
 console.log(`The required list of numbers is ${array}`)
--- task03.ts
+++ task03.ts
@@ -1,9 +1,10 @@
 //Implement a program that uses a loop to iterate through an array of numbers
 //and remove all the even numbers from them and just leave the odd ones
-var array:number[]=[1,2,3,4,5,6,7,8,9,10]
+var array:number[]=[1,2,2,4,5,6,7,8,9,10]
 for(var i=1; i<=array.length;i++){
     if(array[i-1]%2==0){
     array.splice(i-1,1)
+    i--
     }
 }
 console.log(`The required list of numbers is ${array}`)
